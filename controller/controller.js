03de1e181d1cecfc27b89f0460aa08723051505f
jocjoc <89018472+garoxeldiablo@users.noreply.github.com>
2024-06-02 12:15:27 +0700

    userAuth

--- controller/controller.js
+++ controller/controller.js
@@ -1,15 +1,16 @@
 import { query } from "../database/db.js";
 
+// authentication
 export const login = async (req, res) => {
     const { email, password } = req.body;
 
     try {
         // Query untuk menemukan user berdasarkan email dan password
-        const rows = await query('SELECT * FROM user WHERE email = ? AND password = ?', [email, password]);
+        const userAuth = await query('SELECT * FROM user WHERE email = ? AND password = ?', [email, password]);
 
         // Jika username dan password cocok, kirim respon sukses
-        if (rows.length > 0) {
-            res.json({ success: true, message: 'Login successful!' });
+        if (userAuth) {
+            res.json({ success: true, message: 'Login successful!', userAuth });
         } else {
             res.status(401).json({ success: false, message: 'Invalid email or password.' });
         }
@@ -18,3 +19,12 @@ export const login = async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 };
+
+// user
+// export const user = async (req.res) => {
+//     const users = req.body;
+
+//     try {
+//         // tampilkan
+//     }
+// }
