a243b3c14d19109e4770df815ca3c51de8772224
jocjoc <89018472+garoxeldiablo@users.noreply.github.com>
2024-06-20 01:00:21 +0700

    root

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const db = mysql.createPool ({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    database: process.env.DATABASE
+    database: process.env.DATABASE,
 })
 
 
